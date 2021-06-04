75cdbaf2048a06ff52ffcb2995a8175145085014
rysk1013 <rysk2908@gmail.com>
2021-06-05 02:12:21 +0900

    insert table

--- main.go
+++ main.go
@@ -10,12 +10,20 @@ import (
 var Dbconnection *sql.DB
 
 func main() {
+	// テーブル作成
 	Dbconnection, _ := sql.Open("sqlite3", "example.sql")
 	defer Dbconnection.Close()
 	cmd := `CREATE TABLE IF NOT EXISTS person(
 						name STRING,
 						age INT)`
 	_, err := Dbconnection.Exec(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// インサート
+	cmd = "INSERT INTO person (name, age) VALUES (?, ?)"
+	_, err = Dbconnection.Exec(cmd, "Mike", 24)
 	if err != nil {
 		log.Fatalln(err)
 	}
