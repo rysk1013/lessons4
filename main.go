0376e1f3692828757dff22896ec90131e346263e
rysk1013 <rysk2908@gmail.com>
2021-06-05 02:38:01 +0900

    出力方法

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var Dbconnection *sql.DB
+
+// マルチセレクトで使用
+type Person struct {
+	Name string
+	Age int
+}
 
 func main() {
 	// テーブル作成
@@ -23,7 +30,7 @@ func main() {
 
 	// インサート
 	cmd = "INSERT INTO person (name, age) VALUES (?, ?)"
-	_, err = Dbconnection.Exec(cmd, "Mike", 24)
+	_, err = Dbconnection.Exec(cmd, "Nancy", 20)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -34,4 +41,38 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	// マルチセレクト
+	cmd = "SELECT * FROM person"
+	rows, _ := Dbconnection.Query(cmd)
+	defer rows.Close()
+	var pp []Person
+	for rows.Next() {
+		var p Person
+		err := rows.Scan(&p.Name, &p.Age)
+		if err != nil {
+			log.Println(err)
+		}
+		pp = append(pp, p)
+	}
+	// まとめてエラーを出力する
+	err = rows.Err()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, p := range pp {
+		fmt.Println(p.Name, p.Age)
+	}
+
+	// シングルセレクト
+	cmd = "SELECT * FROM person WHERE age = ?"
+	row := Dbconnection.QueryRow(cmd, 20)
+	var p Person
+	err = row.Scan(&p.Name, &p.Age)
+	if err != nil {
+		log.Println("No row")
+	} else {
+		log.Println(err)
+	}
+	fmt.Println(p.Name, p.Age)
 }
